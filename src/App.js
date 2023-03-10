6f04ac9e1f437cbdb3d766b31dc6131bd0c5b937
Iqbal <iamhasan9501@gmail.com>
2023-03-11 02:09:12 +0600

    f

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/user`)
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const phone = parseInt(e.target.phone.value);
+    const user = { name, email, phone };
+
+    fetch(`http://localhost:5000/user`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const newUsers = [...users, user];
+        setUsers(newUsers);
+      })
+      .catch((err) => console.error(err));
+
+    e.target.reset();
+
+    console.log(user);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <form onSubmit={handleAddUser}>
+        <label>Name :</label>
+        <input type="text" name="name" /> <br />
+        <label>Email :</label>
+        <input type="email" name="email" /> <br />
+        <label>Phone :</label>
+        <input type="number" name="phone" min={3} /> <br />
+        <button type="submit">Add User</button>
+      </form>
+      <hr />
+      User total : {users.length}
+      {users.map((user, i) => (
+        <p key={i}>
+          {/* {user._id} */}
+          {user.name}
+          {"  ----"}
+          {user.email}
+          {"  ----"}
+          {user.phone}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ))}
     </div>
   );
 }
